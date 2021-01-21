6e9ca0e607e893371cec8491cdb3f3d3ef55799b
zed-chi <zed-chi@mail.ru>
2021-01-21 16:25:53 +0300

    added item route

--- items/views.py
+++ items/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
+from .models import Item
 
 # Create your views here.
+def get_dict_item(item):
+    return {
+    "id": item.id,
+    "title":item.title,
+    "description": item.description,
+    "image": item.image.url,
+    "weight": item.weight,
+    "price": item.price
+}
+
+def get_item_by_id(req, id):
+    item = get_object_or_404(Item, pk=id)
+    return JsonResponse(get_dict_item(item))
