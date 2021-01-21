12427508ee745fd4fd1c50eca0de09ceea3e762d
zed-chi <zed-chi@mail.ru>
2021-01-21 16:27:59 +0300

    black

--- items/urls.py
+++ items/urls.py
@@ -2,5 +2,5 @@ from django.urls import include, path
 from .views import get_item_by_id
 
 urlpatterns = [
-    path("/<int:id>/", get_item_by_id),    
+    path("/<int:id>/", get_item_by_id),
 ]
--- items/views.py
+++ items/views.py
@@ -5,13 +5,14 @@ from .models import Item
 # Create your views here.
 def get_dict_item(item):
     return {
-    "id": item.id,
-    "title":item.title,
-    "description": item.description,
-    "image": item.image.url,
-    "weight": item.weight,
-    "price": item.price
-}
+        "id": item.id,
+        "title": item.title,
+        "description": item.description,
+        "image": item.image.url,
+        "weight": item.weight,
+        "price": item.price,
+    }
+
 
 def get_item_by_id(req, id):
     item = get_object_or_404(Item, pk=id)
